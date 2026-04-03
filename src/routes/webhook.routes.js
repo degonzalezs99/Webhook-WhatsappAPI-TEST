9f53919afe31b2cf550a13557fd5d1039072ad97
DegonzalezXotrik <dennis.gonzalez@xotrik.com>
2026-04-02 22:11:06 -0600

    Extraer User

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {   verifyWebhook,  receiveMessage, webhookHandler } from "../controllers/webhook.controller.js";
+import {   verifyWebhook,  webhookHandler } from "../controllers/webhook.controller.js";
 
 const router = express.Router();
 
 router.get("/", verifyWebhook);
-router.post("/", receiveMessage);
+//router.post("/", receiveMessage);
 router.post("/webhook", webhookHandler);
 
 
---
+++ src/services/user.service.js
@@ -0,0 +1,9 @@
+export const getUserByPhone = async (phone) => {
+  // luego conectas API o DB
+  return null;
+};
+
+export const createUser = async (user) => {
+  console.log("Creating user:", user);
+  return user;
+};
