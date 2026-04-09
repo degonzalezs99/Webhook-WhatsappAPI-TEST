95f9fdbbbe9da45fb68de1e85ca7d40e2e77b97b
DegonzalezXotrik <dennis.gonzalez@xotrik.com>
2026-04-09 10:37:08 -0600

    Version8-bug

--- src/services/flow.service.js
+++ src/services/flow.service.js
@@ -426,11 +426,13 @@ export const handleFlow = async (user, input) => {
         { id: "1", title: "1" }, { id: "2", title: "2" },
         { id: "3", title: "3" }, { id: "4", title: "4" },
         { id: "5", title: "5" }, { id: "6", title: "6" },
-        { id: "7", title: "7" }, { id: "8", title: "8" },
-        { id: "9", title: "9" }, { id: "10", title: "10" },
+        { id: "7", title: "7" }
       ]);
       break;
     }
+
+
+
 
     case "QUANTITY": {
       if (!isValidOption(input, ["1", "2", "3", "4", "5"])) {
